e4457ec6e50e88bbc61457561e1ef9889cc904b3
jovial-ann <jovialmutua@gmail.com>
2024-02-06 23:58:37 +0300

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Mutua Jovial Ann Wairimu\n");
+    printf("My course is Civil Engineering\n");
+    printf("My favorite team is Manchester City\n");
+    printf("My favorite meal is NYAMA CHOMA\n");
+    return 0;
+}
